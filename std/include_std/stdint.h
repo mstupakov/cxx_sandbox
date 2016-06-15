f81ec5746f25827a49bec163d4b03d869c94793f
Maksym Stupakov <maksym.stupakov@gmail.com>
2016-06-15 16:21:08 +0300

    Fixed STD header dependency in stdint.h.

---
+++ std/include_std/stdint.h
@@ -0,0 +1,16 @@
+/*
+ * This file is part of "cxx_sandbox" application.
+ *
+ * Copyright © 2016 Maksym Stupakov <maksym.stupakov@gmail.com>
+ * This file is free software, distributed under the MIT License.
+ */
+
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
+/* uSTL requres the defines */
+#define __STDC_LIMIT_MACROS
+#define __STDC_CONSTANT_MACROS
+#include_next <stdint.h>
+
+#endif /* __STDINT_H__ */
